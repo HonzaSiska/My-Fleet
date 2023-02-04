ea5e56bdc325898cc611d86ac56e8371ddd31891
HonzaSiska <siskajan@hotmail.com>
2023-02-04 16:15:18 +0100

    added log

--- backend/server.js
+++ backend/server.js
@@ -31,17 +31,7 @@ app.use('/api/fuel', fuelRoutes)
 app.use('/api/maintenance', maintenanceRoutes)
 app.use('/api/stats', statsRoutes)
 
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err) {
-          if(err) {
-              res.status(500).send(err)
-          }
-      });
-  })
-}
+
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
@@ -49,6 +39,17 @@ mongoose.connect(process.env.MONGO_URI)
     // listen for requests
     app.listen(process.env.PORT || 4000, () => {
       console.log('connected to db & listening on port', process.env.PORT)
+      if (process.env.NODE_ENV === "production") {
+        const path = require("path");
+        app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
+        app.get("*", (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err) {
+                if(err) {
+                    res.status(500).send(err)
+                }
+            });
+        })
+      }
     })
     
   })
